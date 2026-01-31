37445f9b3d9358d09e55ac11fc60a7b1b9d65f29
raudhra <rudra888pratap@gmail.com>
2026-01-31 17:17:21 +0530

    Added getBooksById controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,8 +2,12 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
+	"gorilla/mux"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/raudhra/book-management-system/models"
 )
 
@@ -12,6 +16,20 @@ var NewBooks models.Books
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookID := vars["bookID"]
+	ID, err := strconv.ParseInt(bookID, 0, 0)
+	if err != nil {
+		fmt.Println("Error")
+	}
+	bookDetails := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
