a343bcd0990876c840fcdbb25fc2fe6f86ca4ccc
raudhra <rudra888pratap@gmail.com>
2026-01-31 17:00:49 +0530

    Get all books func in book-controllers

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,18 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/raudhra/book-management-system/models"
+)
+
+var NewBooks models.Books
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
